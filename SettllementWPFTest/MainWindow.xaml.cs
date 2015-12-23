79e9610ebe74f489ed0aba90162ab3b2c9aaa247
biggreyhairboy <biggreyhairboy@gmail.com>
2015-12-24 00:24:04 +0800

    基本结构出来了，第一次推送，菜单栏从中分开的问题需要解决

---
+++ SettllementWPFTest/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Reflection;
+
+namespace SettllementWPFTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Assembly Asm = Assembly.GetEntryAssembly();
+
+            string AppInfo = Asm.GetName().Version.ToString() ;
+            MessageBox.Show(AppInfo);
+        }
+
+        
+    }
+}
